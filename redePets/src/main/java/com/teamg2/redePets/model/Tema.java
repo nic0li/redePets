4211140e612fff384502b8b83c380a7d98406bc5
Nicoli <nicoli1992@gmail.com>
2021-03-22 11:42:28 -0300

    add: model da tabela tema

---
+++ redePets/src/main/java/com/teamg2/redePets/model/Tema.java
@@ -0,0 +1,64 @@
+package com.teamg2.redePets.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 1, max = 100)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 1, max = 500)
+	private String descricao;
+	
+	@NotNull
+	private long relevancia;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public long getRelevancia() {
+		return relevancia;
+	}
+
+	public void setRelevancia(long relevancia) {
+		this.relevancia = relevancia;
+	}
+	
+	
+
+}
