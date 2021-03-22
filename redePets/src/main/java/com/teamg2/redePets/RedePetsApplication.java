501848c0a0f5ecd841ca422c48b9c2525f84d4a7
Nicoli <nicoli1992@gmail.com>
2021-03-22 10:22:39 -0300

    add: estrutura inicial do projeto spring boot

---
+++ redePets/src/main/java/com/teamg2/redePets/RedePetsApplication.java
@@ -0,0 +1,13 @@
+package com.teamg2.redePets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedePetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedePetsApplication.class, args);
+	}
+
+}
---
+++ redePets/src/test/java/com/teamg2/redePets/RedePetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamg2.redePets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedePetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
