a979df07b2df442369ce5f6eccb8bcb910d4aba1
Karina Souza <souzakarina098@gmail.com>
2021-04-09 12:31:25 -0300

    add: camada de UsuarioController

---
+++ redePets/src/main/java/com/teamg2/redePets/controller/UsuarioController.java
@@ -0,0 +1,47 @@
+package com.teamg2.redePets.controller;
+
+import java.util.Optional;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.teamg2.redePets.model.Usuario;
+import com.teamg2.redePets.model.UsuarioLogin;
+import com.teamg2.redePets.service.UsuarioService;
+
+@RestController
+@CrossOrigin(origins = "*",allowedHeaders = "*")
+@RequestMapping("/usuarios")
+public class UsuarioController {
+	
+	@Autowired
+	public UsuarioService usuarioService;
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> Autentication(@RequestBody Optional <UsuarioLogin> usuarioLogin){
+		return usuarioService.Logar(usuarioLogin).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+		
+	}
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
+		Optional<Usuario> user = usuarioService.CadastrarUsuario(usuario);
+
+        try {
+            return ResponseEntity.ok(user.get());
+        } 
+        catch (Exception err) {
+            return ResponseEntity.badRequest().build();
+        }
+	}
+	
+
+}
