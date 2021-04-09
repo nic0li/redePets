9f489053cecbf7068feeac1e4802dec125fbfa3d
Nicoli <nicoli1992@gmail.com>
2021-04-09 10:09:50 -0300

    add: dependência de segurança, model de login do usuário e repository de usuário

--- redePets/src/main/java/com/teamg2/redePets/model/Usuario.java
+++ redePets/src/main/java/com/teamg2/redePets/model/Usuario.java
@@ -23,15 +23,15 @@ public class Usuario {
 	private long id;
 	
 	@NotNull
-	@Size(min = 1, max = 100)
-	private String nome_completo;
+	@Size(min = 2, max = 100)
+	private String nomeCompleto;
 	
 	@NotNull
-	@Size(min = 1, max = 100)
+	@Size(min = 2, max = 100)
 	private String email;
 	
 	@NotNull
-	@Size(min = 1, max = 100)
+	@Size(min = 5, max = 100)
 	private String senha;
 	
 	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
@@ -46,12 +46,12 @@ public class Usuario {
 		this.id = id;
 	}
 
-	public String getNome_completo() {
-		return nome_completo;
+	public String getNomeCompleto() {
+		return nomeCompleto;
 	}
 
-	public void setNome_completo(String nome_completo) {
-		this.nome_completo = nome_completo;
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
 	}
 
 	public String getEmail() {
