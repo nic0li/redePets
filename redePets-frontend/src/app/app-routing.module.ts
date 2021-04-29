9094100f6db005bf05f2807531a859ae3a44fdd9
Diego <diegoti231@gmail.com>
2021-04-29 13:33:00 -0300

    add: criando rotas

--- redePets-frontend/src/app/app-routing.module.ts
+++ redePets-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { InicioDeslogadoComponent } from './inicio-deslogado/inicio-deslogado.component';
+import { InicioLogadoComponent } from './inicio-logado/inicio-logado.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path: '', redirectTo: 'inicio-deslogado', pathMatch: 'full'},
+  {path: 'entrar', component: EntrarComponent },
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'inicio-deslogado', component: InicioDeslogadoComponent},
+  {path: 'inicio-logado', component: InicioLogadoComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'rodape', component: RodapeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
