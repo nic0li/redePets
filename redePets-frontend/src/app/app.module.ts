82cdddd3b3a953669306dc28c64a9b30169c140c
Nicoli <nicoli1992@gmail.com>
2021-04-29 10:06:26 -0300

    add: estrutura inicial do angular + estrutura dos componentes menu e rodape

---
+++ redePets-frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
